82baa1398b5657297ffe77a47fbfc344c0e79594
komehta <komehta@IN-komehta-W1.adprod.bmc.com>
2019-11-11 21:42:51 +0530

    Added Data Provider for fetching Test Data.

--- RestAssured/src/main/java/RestAssuredProject/AbusiveExperienceAPI.java
+++ RestAssured/src/main/java/RestAssuredProject/AbusiveExperienceAPI.java
@@ -18,9 +18,11 @@ public class AbusiveExperienceAPI {
 		
 		then().assertThat().statusCode(200).extract().response();
 		
+		System.out.println("Violiting Sites resposne is : " +res.asString());
+		
 	}
 	
-	public void getAbusiveStatus() {
+	public void getAbusiveStatus(String website) {
 		
 		RestAssured.baseURI="https://abusiveexperiencereport.googleapis.com";
 		
@@ -28,9 +30,11 @@ public class AbusiveExperienceAPI {
 			queryParam("key", "AIzaSyCijvqCvlY1bjdB-h8lELdT571jBXr6vqU").
 		
 		when().
-		get("/v1/sites/http%3A%2F%2Fwww.google.com%2F.").
+		get("/v1/sites/http%3A%2F%2Fwww."+website+".com%2F.").
 		
 		then().assertThat().statusCode(200).extract().response();
+		
+		System.out.println("Abusive Status Response is : " +res.asString());
 	}
 	
 	
