7c391e865b8da706da4f0054ea579768d96e2faa
komehta <komehta@IN-komehta-W1.adprod.bmc.com>
2019-11-20 15:50:11 +0530

    Added the assertions in GmailAPI class.
    Needs fixes for two post methods.

--- RestAssured/src/main/java/RestAssuredProject/GmailAPI.java
+++ RestAssured/src/main/java/RestAssuredProject/GmailAPI.java
@@ -3,14 +3,27 @@ package RestAssuredProject;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import static org.hamcrest.Matchers.*;
 
 import static io.restassured.RestAssured.given;
+
+import java.util.concurrent.TimeUnit;
+
+import org.testng.Assert;
 
 public class GmailAPI {
 	
 	public static String messageId;
 	public static String draftMessageId;
+	public JsonPath jsonRes;
 	
+	
+	/*****
+	 * Get's the user details with total Message in inbox
+	 * @validations Timeout,Header,NodeValue
+	 * @param email
+	 * @param AccessToken
+	 */
 	public void getUserProfile(String email,String AccessToken) {
 		
 		RestAssured.baseURI="https://www.googleapis.com";
@@ -23,14 +36,30 @@ public class GmailAPI {
 		when().
 		get("/gmail/v1/users/"+email+"/profile").
 		
-		then().assertThat().statusCode(200).extract().response();
+		then().assertThat()
+		.statusCode(200)
+		.time(lessThan(4l), TimeUnit.SECONDS)
+		.header("Vary", "X-Origin")
+		.header("Server", "GSE")
+		.header("X-Frame-Options", "SAMEORIGIN")
+		.body("messagesTotal", not(hasSize(0)))
+		.extract().response();
+		
+		jsonRes=new JsonPath(res.asString());
+		System.out.println("Total Number of Messages in your inbox is : " +jsonRes.get("messagesTotal"));
 		
 		System.out.println("User profile response : " +res.asString());
 	}
 	
+	/*******
+	 * Provides the search result of email 
+	 * with the given search keyword and fetches
+	 * the messageID to be used in other locations
+	 * @param email
+	 * @param AccessToken
+	 */
 	
-	
-	public void listUserMessages(String email,String AccessToken) {
+	public void listUserMessages(String email,String AccessToken, String Query) {
 		
 		RestAssured.baseURI="https://www.googleapis.com";
 		
@@ -38,21 +67,33 @@ public class GmailAPI {
 		.auth().oauth2(AccessToken)
 		.queryParam("includeSpamTrash", false)
 		.queryParam("maxResults", 2)
-		.queryParam("q", "from:greetings@travel.e-redbus.in").
+		.queryParam("q", Query).
 		
 		when()
 		.get("/gmail/v1/users/"+email+"/messages").
 		
 		then()
-		.assertThat().statusCode(200).extract().response();
+		.assertThat()
+		.statusCode(200)
+		.time(lessThan(4000l))
+		.body("resultSizeEstimate",not(hasSize(0)))
+		.extract().response();
 		
-		JsonPath resPath = new JsonPath(res.asString());
-		messageId = resPath.get("messages[0].id");
-		
+		jsonRes = new JsonPath(res.asString());
+		messageId = jsonRes.get("messages[0].id");
+		System.out.println("Result Size Estimate is : "+jsonRes.get("resultSizeEstimate").toString());
 		System.out.println("Response from List User Messages : " +res.asString());
 	}
 	
-	
+	/*******
+	 * gets a particular message details when
+	 * provided with message ID from list user
+	 * messages request
+	 * @Validations Timeout,NodeValue and StatusCode
+	 * and testNG Assertions
+	 * @param Email
+	 * @param AccessToken
+	 */
 	public void getUserMessages(String Email, String AccessToken) {
 		RestAssured.baseURI="https://www.googleapis.com";
 		Response res = given().auth().oauth2(AccessToken)
@@ -60,14 +101,31 @@ public class GmailAPI {
 		.pathParam("userId", Email)
 		.queryParam("format", "raw").
 		when()
-		.get("gmail/v1/users/{userId}/messages/{id}").
+		.get("/gmail/v1/users/{userId}/messages/{id}").
 		then()
-		.assertThat().statusCode(200).extract().response();
+		.assertThat()
+		.statusCode(200)
+		.time(lessThan(4000l))
+		.body("snippet", not(hasSize(0)))
+		.header("Cache-Control", "private, max-age=0, must-revalidate, no-transform")
+		.extract().response();
 		
+		jsonRes = new JsonPath(res.asString());
+		int sizeofLabels = res.jsonPath().getList("labelIds").size();
+		Assert.assertEquals(sizeofLabels, 3);
+		System.out.println("Message Details are as follows : "+jsonRes.get("snippet").toString());
+		System.out.println("Size of the message : "+jsonRes.get("sizeEstimate").toString());
 		System.out.println("Response for getUser Messages is : " +res.asString());
 	}
 
-	
+	/*****
+	 * Deletes the email from Gmail
+	 * when provided with the thread Id
+	 * using list messages request
+	 * @validations 
+	 * @param email
+	 * @param AccessToken
+	 */
 	public void deletMessageFromGmail(String email,String AccessToken) {
 		RestAssured.baseURI="https://www.googleapis.com";
 		Response res = given().auth().oauth2(AccessToken)
@@ -76,29 +134,57 @@ public class GmailAPI {
 		when()
 		.post("/gmail/v1/users/{userId}/messages/{id}/trash").
 		then()
-		.assertThat().statusCode(200).extract().response();
+		.assertThat()
+		.statusCode(200)
+		.time(lessThan(4000l))
+		.extract().response();
+		
+		jsonRes = new JsonPath(res.asString());
+		Assert.assertEquals(jsonRes.get("id").toString(), messageId);
 		
 		System.out.println("Response for deleteMessage From Gmail is : " +res.asString());
 		
 	}
 	
-	public void sendMessageFromGmail(String email, String toEmail, String MessageText, String AccessToken) {
+	/*****
+	 * Sends an email to an recipient in To address
+	 * @validations Status code and time Out
+	 * @param email
+	 * @param toEmail
+	 * @param MessageText
+	 * @param AccessToken
+	 */
+	public void sendMessageFromGmail(String email, String toEmail, 
+			String MessageText, String AccessToken) {
 		RestAssured.baseURI="https://www.googleapis.com";
 		Response res = given().auth().oauth2(AccessToken)
 		.header("To", toEmail)
 		.pathParam("userId", email)
 		.queryParam("uploadType", "media")
 		.body("{\r\n" + 
-				"  \"raw\":"+ MessageText+"\r\n" + 
+				"  \"raw\": \""+MessageText+"\"\r\n" + 
 				"}").
 		when()
 		.post("/gmail/v1/users/{userId}/messages/send").
 		then()
-		.assertThat().statusCode(200).extract().response();
+		.assertThat()
+		.statusCode(200)
+		.time(lessThan(4000l))
+		.extract().response();
 		
 		System.out.println("Response for Send message from Gmail is : " +res.asString());
 		
 	}
+	
+	/********
+	 * List down drafts messages in 
+	 * user gmail and asserts that the
+	 * result size equals to MaxResults
+	 * provided by user in query
+	 * @validations 
+	 * @param email
+	 * @param AccessToken
+	 */
 	
 	public void listUserDraftMessages(String email,String AccessToken) {
 		RestAssured.baseURI="https://www.googleapis.com";
@@ -108,16 +194,27 @@ public class GmailAPI {
 		when()
 		.get("/gmail/v1/users/{userId}/drafts").
 		then()
-		.assertThat().statusCode(200).extract().response();
+		.assertThat()
+		.time(lessThan(4000l))
+		.statusCode(200)
+		.extract().response();
 		
-		JsonPath respath = new JsonPath(res.asString());
-		draftMessageId= respath.get("drafts[0].id");
+		jsonRes = new JsonPath(res.asString());
+		draftMessageId= jsonRes.get("drafts[0].id");
+		
+		int sizeofResults = res.jsonPath().getList("drafts").size();
+		Assert.assertEquals(sizeofResults, 2);
 		
 	}
 	
+	/********
+	 * Get the draft message details from gmail
+	 * @validations Timeout,StatusCode,NodeValue
+	 * @param email
+	 * @param AccessToken
+	 */
 	public void getDraftFromGmail(String email, String AccessToken) {
 		RestAssured.baseURI="https://www.googleapis.com";
-		
 		Response res = given().auth().oauth2(AccessToken)
 		.pathParam("id", draftMessageId)
 		.pathParam("userId", email )
@@ -125,11 +222,27 @@ public class GmailAPI {
 		when()
 		.get("/gmail/v1/users/{userId}/drafts/{id}").
 		then()
-		.assertThat().statusCode(200).extract().response();
+		.assertThat()
+		.statusCode(200)
+		.time(lessThan(4000l))
+		.body("snippet", not(hasSize(0)))
+		.extract().response();
+		jsonRes = new JsonPath(res.asString());
+		
+		Assert.assertEquals(jsonRes.get("id").toString(), draftMessageId);
+		
+			System.out.println("Details of Draft message is as follows : "+jsonRes.get("snippet"));
 		
 		System.out.println("Response for Get draft from Gmail is : " +res.asString());
 	}
 	
+	
+	/*****
+	 * Deletes the given draft from Gmail
+	 * @validations Timeout, NodeValue, Status Code
+	 * @param email
+	 * @param AccessToken
+	 */
 	public void deleteDraftFromGmail(String email,String AccessToken) {
 		RestAssured.baseURI="https://www.googleapis.com";
 		
@@ -139,11 +252,22 @@ public class GmailAPI {
 		when()
 		.delete("/gmail/v1/users/{userId}/drafts/{id}").
 		then()
-		.assertThat().statusCode(204).extract().response();
+		.assertThat()
+		.statusCode(204)
+		.time(lessThan(4000l))
+		.extract().response();
 		
+		jsonRes = new JsonPath(res.asString());
 		System.out.println("Response for Delete Draft from Gmail : " +res.asString());
 	}
 	
+	/****
+	 * Creates a draft email in Gmail account
+	 * @validations Timeout, NodeValue, StatusCode
+	 * @param email
+	 * @param message
+	 * @param AccessToken
+	 */
 	public void createDraftinGmail(String email, String message, String AccessToken) {
 		RestAssured.baseURI="https://www.googleapis.com";
 		
@@ -152,14 +276,17 @@ public class GmailAPI {
 		.queryParam("uploadType", "media")
 		.body("{\r\n" + 
 				"  \"message\": {\r\n" + 
-				"    \"raw\": \"DraftMessage\"\r\n" + 
+				"    \"raw\": \""+message+"\",\r\n" + 
 				"  }\r\n" + 
 				"}").
 		when()
 		.post("/upload/gmail/v1/users/{userId}/drafts").
 		then()
-		.assertThat().statusCode(200).extract().response();
-		
+		.assertThat()
+		.statusCode(200)
+		.time(lessThan(4000l))
+		.body("id",is(not(null)))
+		.extract().response();
 		System.out.println("Response for createDraft in gmail is  : " +res.asString());
 	}
 	
--- RestAssured/src/test/java/RestAssuredProject/GmailAPITest.java
+++ RestAssured/src/test/java/RestAssuredProject/GmailAPITest.java
@@ -10,18 +10,18 @@ public class GmailAPITest extends BaseClass {
 	 * the AuthToken to complete the OAUTH2.0 
 	 * @throws Exception
 	 */
-	@BeforeTest
-	public void gettingAuthToken() throws Exception {
-		BaseClass.constructAuthenticationURL("https://accounts.google.com","/o/oauth2/v2/auth","https://mail.google.com/",
-				"https://accounts.google.com/o/oauth2/auth",
-				"745077292106-oihr15fk3od8mn5i12eb1jlkk67352eh.apps.googleusercontent.com",
-				"code","https://www.getpostman.com/oauth2/callback",
-				"empty");
-		BaseClass.driverInitialization();
-		BaseClass.getCodeThroughBrowserAuthentication();
-		BaseClass.driverTearDown();
-		BaseClass.getBearerAccessToken();
-	}
+//	@BeforeTest
+//	public void gettingAuthToken() throws Exception {
+//		BaseClass.constructAuthenticationURL("https://accounts.google.com","/o/oauth2/v2/auth","https://mail.google.com/",
+//				"https://accounts.google.com/o/oauth2/auth",
+//				"745077292106-oihr15fk3od8mn5i12eb1jlkk67352eh.apps.googleusercontent.com",
+//				"code","https://www.getpostman.com/oauth2/callback",
+//				"empty");
+//		BaseClass.driverInitialization();
+//		BaseClass.getCodeThroughBrowserAuthentication();
+//		BaseClass.driverTearDown();
+//		BaseClass.getBearerAccessToken();
+//	}
 	
 	
 	
@@ -36,49 +36,73 @@ public class GmailAPITest extends BaseClass {
 		TS1.getUserProfile(Username,AccessToken);
 	}
 	
-	
+	/****
+	 * API Docs : https://developers.google.com/gmail/api/v1/reference/users/messages/list
+	 */
 	@Test(priority=2,enabled=true)
 	public void listUserMessages() {
 		GmailAPI TS1 = new GmailAPI();
-		TS1.listUserMessages(Username,AccessToken);
+		TS1.listUserMessages(Username,AccessToken,"from:greetings@travel.e-redbus.in");
 	}
 	
+	/****
+	 * API Docs : https://developers.google.com/gmail/api/v1/reference/users/messages/get
+	 */
 	@Test(priority=3,enabled=true)
 	public void getUserMessages() {
 		GmailAPI TS1 = new GmailAPI();
 		TS1.getUserMessages(Username, AccessToken);
 	}
 	
+	/*****
+	 * API Docs : https://developers.google.com/gmail/api/v1/reference/users/messages/trash
+	 */
 	@Test(priority=4,enabled=true)
 	public void deleteMessageFromGmail() {
 		GmailAPI TS1 = new GmailAPI();
 		TS1.deletMessageFromGmail(Username, AccessToken);
 	}
 	
+	/*****
+	 * API Docs : https://developers.google.com/gmail/api/v1/reference/users/messages/send
+	 */
 	@Test(priority=5,enabled=true)
 	public void sendEmailfromGmail() {
 		GmailAPI TS1 = new GmailAPI();
 		TS1.sendMessageFromGmail(Username, Username, "This message is sent from Rest Assured.", AccessToken);
 	}
 	
+	
+	/***
+	 * API Docs : https://developers.google.com/gmail/api/v1/reference/users/messages/send
+	 */
 	@Test(priority=6,enabled=true)
 	public void listUserDraftMessage() {
 		GmailAPI TS1 = new GmailAPI();
 		TS1.listUserDraftMessages(Username, AccessToken);
 	}
 	
+	/***
+	 * API Docs : https://developers.google.com/gmail/api/v1/reference/users/drafts/get
+	 */
 	@Test(priority=7,enabled=true)
 	public void getDraftMessage() {
 		GmailAPI TS1 = new GmailAPI();
 		TS1.getDraftFromGmail(Username, AccessToken);
 	}
 	
+	/***
+	 * API Docs : https://developers.google.com/gmail/api/v1/reference/users/drafts/delete
+	 */
 	@Test(priority=8,enabled=true)
 	public void deleteDraftMessage() {
 		GmailAPI TS1 = new GmailAPI();
 		TS1.deleteDraftFromGmail(Username, AccessToken);
 	}
 	
+	/****
+	 * API Docs : 
+	 */
 	@Test(priority=9,enabled=true)
 	public void createDraftinGmail() {
 		GmailAPI TS1 = new GmailAPI();
